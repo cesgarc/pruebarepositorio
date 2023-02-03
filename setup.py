2e6f580a1805645ec06fbeff322639d8da849407
César García <cesgarc@gmail.com>
2023-02-03 10:55:16 -0500

    Primer Commit de Proyecto

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='breastcancerproject',
+    packages=find_packages(),
+    description='Breast Cancer Detection',
+    author='Computer Vision Team Humai',
+    version='0.0.1'
+)
